23ff197e737da446cd22380968c8743e9999731a
Bekzod <bekzodisakov18@gmail.com>
2023-06-07 00:48:04 +0500

    add .gitlab-ci.yml file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.DEV ? "/" : "/users-admin/"}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/users-admin/'
-  }
-
-  return config
+export default defineConfig({
+  base: "/users-admin/",
+  plugins: [react()],
 })
